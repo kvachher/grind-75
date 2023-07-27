6edd6faeb1b2868a8f606f306a694fb00dfdd1bf
Kush Vachher <kush.vachher@gmail.com>
2023-07-27 15:59:14 -0400

    Create maximum-depth-of-binary-tree.java

---
+++ Easy/maximum-depth-of-binary-tree.java
@@ -0,0 +1,29 @@
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        int l = 0; 
+        int r = 0; 
+        if (root == null)
+            return 0; 
+        if (root.left == null && root.right == null)
+            return 1;
+        if (root.left != null)
+            l = maxDepth(root.left);
+        if (root.right != null) 
+            r = maxDepth(root.right);
+
+        return 1 + Math.max(l, r);
+    }
+}
