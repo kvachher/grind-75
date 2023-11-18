b7cbb17ce0694067a3ee6aa8bc259f04ccb645d6
Kush Vachher <kush.vachher@gmail.com>
2023-11-18 17:47:52 -0500

    Create binary-tree-level-order-traversal.py

---
+++ Medium/binary-tree-level-order-traversal.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def levelOrder(self, root):
+        ans, level = [], [root]
+        while root and level:
+            ans.append([node.val for node in level])
+            LRpair = [(node.left, node.right) for node in level]
+            level = [leaf for LR in LRpair for leaf in LR if leaf]
+        return ans
